e470da93d1f52bf77f825c5fad974246aeb3da93
Vill4!n'S Team <64704348+Vill4inS@users.noreply.github.com>
2020-05-13 13:38:24 -0700

    Install file

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 apt update
 apt-get install -y figlet
 apt-get install nmap
+apt-get install eyewitness
  
 
 go get -u github.com/tomnomnom/assetfinder
@@ -10,4 +11,3 @@ go get -v github.com/OWASP/Amass/v3/...
 go get -u github.com/tomnomnom/httprobe
 go get github.com/haccer/subjack
 go get github.com/tomnomnom/waybackurls
-
