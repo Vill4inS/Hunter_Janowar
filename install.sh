3a154295fb7cf21c9826c6be6dab6fc6b91e1947
Vill4!n'S Team <64704348+Vill4inS@users.noreply.github.com>
2020-05-17 12:25:32 -0700

    Install file

--- install.sh
+++ install.sh
@@ -1,13 +1,33 @@
-#!/bin/bash
+apt-get install figlet -y
+figlet -f slant " Vill4!n'S Team"
+echo "                              Every Villain is a Hero in his Own Mind "
+echo "#====================================================================#"
+echo "#||||||||||||||||||||||  Coded by MR_VILLAIN  |||||||||||||||||||||||#"
+echo "#######################  We are the Vill4!n'S ########################"
+echo "#--------------------------------------------------------------------#"
+echo "#                  TOOLS INSTALLER - Hunter_Janowar                  #"
+echo "#====================================================================#"
+echo " [*] Installing Essential Tools..."
+echo " [*] Please Wait  "
+apt-get update -y &&
+echo " [*] Installing Eyewitness... "
+apt-get install eyewitness -y 
+echo " [*] Installing Assetfinder... "
+go get -u github.com/tomnomnom/assetfinder 
+echo " [*] Installing Amass... "
+go get -v github.com/OWASP/Amass/v3/... 
+echo " [*] Installing httprobe... "
+go get -u github.com/tomnomnom/httprobe 
+echo " [*] Installing Subjack... "
+go get github.com/haccer/subjack 
+echo " [*] Installing Waybackurl... "
+go get github.com/tomnomnom/waybackurl 
 
-apt update
-apt-get install -y figlet
-apt-get install nmap
-apt-get install eyewitness
- 
+touch ~/.bash_profile
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile
+source ~/.bash_profile
+#If not Work, Then try it...
+#go env -w GOPATH=$HOME/go
 
-go get -u github.com/tomnomnom/assetfinder
-go get -v github.com/OWASP/Amass/v3/...
-go get -u github.com/tomnomnom/httprobe
-go get github.com/haccer/subjack
-go get github.com/tomnomnom/waybackurls
+echo " [*] Installation Done "
+echo " [*] ./Paece " 
